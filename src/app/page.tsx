72f8da14b3944c7cbba376313f62963bf0d761ea
Zhifan Chen <me@zhifan.me>
2023-09-01 22:43:30 +0200

    feat(page): list movies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,85 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+type Movie = {
+  id: string;
+  title: string;
+  year: string;
+  runtime: string;
+  genres: string;
+  director: string;
+  actors: string[];
+  plot: string;
+  posterUrl: string;
+};
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+type Node = {
+  node: Movie;
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+export async function fetchEdges() {
+  try {
+    const response = await fetch(process.env.GRAPHQL_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        apikey: process.env.API_KEY,
+      },
+      body: JSON.stringify({
+        query: `
+  {
+    moviesCollection {
+      edges {
+        node {
+          id
+          title
+          year
+          runtime
+          genres
+          director
+          actors
+          plot
+          posterUrl
+        }
+      }
+    }
+  }
+    `,
+      }),
+      next: { revalidate: 10 },
+    });
+    const { data } = await response.json();
+    const { moviesCollection } = data;
+    const { edges } = moviesCollection as { edges: Node[] };
+    return edges;
+  } catch (e) {
+    console.log("error", e);
+    return [];
+  }
 }
+
+type MovieEntryProps = {
+  movie: Movie;
+};
+
+const MovieEntry = ({ movie }: MovieEntryProps) => {
+  return (
+    <div key={movie.id} className="movieEntry">
+      <h2>{movie.title}</h2>
+      <img src={movie.posterUrl} alt="No image provided" />
+      <p>{movie.plot}</p>
+      <button>Favourite</button>
+    </div>
+  );
+};
+
+const Movies = async () => {
+  const edges = await fetchEdges();
+  return (
+    <div>
+      <h1>MOVIES</h1>
+      {edges.map((edge) => (
+        <MovieEntry key={edge.node.id} movie={edge.node} />
+      ))}
+    </div>
+  );
+};
+
+export default Movies;
