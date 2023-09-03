bc2eb2526f91ffffbe909e09cf6b786480ba0f48
Zhifan Chen <me@zhifan.me>
2023-09-03 14:55:55 +0200

    feat(MovieEntry): layout

--- src/components/MovieEntry/MovieEntry.tsx
+++ src/components/MovieEntry/MovieEntry.tsx
@@ -16,25 +16,31 @@ export const MovieEntry = ({ movie }: MovieEntryProps) => {
 
   return (
     <div key={movie.id} className="movieEntry">
-      <h2>{movie.title}</h2>
-      <img src={movie.posterUrl} alt="No image provided" />
-      <p>{movie.plot}</p>
-      <button
-        onClick={() => {
-          let message;
-          if (isFavorite) {
-            removeFavorite(movie);
-            message = `Removed '${movie.title}' from favorites`;
-          } else {
-            addFavorite(movie);
-            message = `Added '${movie.title}' to favorites`;
-          }
-          setIsFavorite(!isFavorite);
-          addNotification(message);
-        }}
-      >
-        {isFavorite ? "Remove from favorites" : "Add to favorites"}
-      </button>
+      <h2 className="movieEntryHeader">{movie.title}</h2>
+      <img
+        className="movieEntryImage"
+        src={movie.posterUrl}
+        alt="No image provided"
+      />
+      <div className="movieEntryInfo">
+        <p>{movie.plot}</p>
+        <button
+          onClick={() => {
+            let message;
+            if (isFavorite) {
+              removeFavorite(movie);
+              message = `Removed '${movie.title}' from favorites`;
+            } else {
+              addFavorite(movie);
+              message = `Added '${movie.title}' to favorites`;
+            }
+            setIsFavorite(!isFavorite);
+            addNotification(message);
+          }}
+        >
+          {isFavorite ? "Remove from favorites" : "Add to favorites"}
+        </button>
+      </div>
     </div>
   );
 };
