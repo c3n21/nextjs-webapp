05702bc22fd98b6ad6d8a1923d38531323ff294f
Zhifan Chen <me@zhifan.me>
2023-09-04 08:09:29 +0200

    feat(other): phone number regexp

--- src/other/index.ts
+++ src/other/index.ts
@@ -198,6 +198,50 @@ for (const testInput of httpUrlValidatorTestInputs) {
   console.log(
     `[URL VALIDATOR TEST RESULT] ::: Test Input: [${testInput}], Test Passed: ${urlValidator(
       testInput
-    )}`
+    )}\n\n`
+  );
+}
+
+const phoneNumberExtractorTestInputs = [
+  ["My phone number is (039) 02-121212 ", ["(039) 02-121212"]],
+  [
+    "None of my 32 friends has a phone. One of them gave me the number 123-123123 but it isn't valid ",
+    [],
+  ],
+  [
+    "Phone numbers cannot have letters. Numbers like (800) STAR-121212 are not valid ",
+    [],
+  ],
+  [
+    "Every country has its own rules for phone numbers. Even a number like (1) 1-2 can be a valid phone number",
+    ["(1) 1-2"],
+  ],
+] as const;
+
+function arrayCompare<T>(
+  arr1: T[],
+  arr2: T[],
+  equalFn: (left: T, right: T) => boolean = (left, right) => left === right
+): boolean {
+  return (
+    arr1.length === arr2.length &&
+    arr1.reduce((acc, value, index) => {
+      return acc && equalFn(value, arr2[index]);
+    }, true)
+  );
+}
+
+function phoneNumberExtractor(input: string): string[] {
+  const phoneNumberRegex = /\(\d+\)\s\d+-\d+/g;
+  return phoneNumberRegex.exec(input) ?? [];
+}
+
+for (const [testInput, testExpectation] of phoneNumberExtractorTestInputs) {
+  const testResult = phoneNumberExtractor(testInput);
+  console.log(
+    `[PHONE NUMBER EXTRACTOR TEST RESULT] ::: Test Input: [${testInput}], Test Result : [${testResult}], Test Passed: ${arrayCompare(
+      testResult,
+      testExpectation as unknown as string[]
+    )}\n\n`
   );
 }
